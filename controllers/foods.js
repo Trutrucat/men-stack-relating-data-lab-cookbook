887821e0904ef2ddf366d803a4e537bba79f2bad
Trutrucat <167733097+Trutrucat@users.noreply.github.com>
2024-08-09 10:15:21 -0700

    edit and update methods added user controller and views added

--- controllers/foods.js
+++ controllers/foods.js
@@ -23,23 +23,78 @@ router.get('/new', (req, res) => {
     res.render('foods/new.ejs',{ user: req.session.user })  
 });
 
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const userId = req.session.user._id
+        const itemId = req.params.id
+        const user = await User.findById(userId)
+        
+        if (!user) {
+            return res.redirect('/')
+        }
+
+        const pantryItem = user.pantry.id(itemId)
+        if (!pantryItem) {
+            return res.redirect('/')
+        }
+        res.render('edit.ejs', { user: req.session.user, food: pantryItem })    
+    }catch (error) {
+        console.error(error)
+        res.redirect('/')   
+    }
+})
+router.put('/:id', async (req, res) => {
+    try {
+        const userId = req.session.user._id
+        const itemId = req.params.id
+        const user = await User.findById(userId)
+        if(!user) {
+            return res.redirect('/')
+        }
+        const pantryItem = user.pantry.id(itemId)
+        if (!pantryItem) {
+            return res.redirect('/')
+        }
+        pantryItem.food = req.body.food
+        pantryItem.isInPantry = req.body.isInPantry === 'on' || req.body.isInPantry === true
+        await user.save()
+        res.redirect(`/users/${userId}/foods`)
+    } catch (error) {
+        console.error(error)
+        res.redirect('/')
+    }
+})
+
 router.post('/', async (req, res) => {
-    try { 
+   
+        req.body.isInPantry === 'on' || req.body.isInPantry === true?
+        req.body.isInPantry = true:
+        req.body.isInPantry = false 
+        try { 
         const foundUser = await User.findById(req.session.user._id)
-        if (!foundUser) {
-            return res.redirect('/');
-        }
-        const newFoodItem = {
-            food: req.body.food,
-            isInPantry: req.body.isInPantry === 'true'
-        }
-    
-        foundUser.pantry.push(newFoodItem)
+        foundUser.pantry.push(req.body)
         await foundUser.save()
         res.redirect(`/users/${foundUser._id}/foods`)
     } catch (error) {
         res.redirect('/')
     }
 });
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const userId = req.session.user._id
+        const itemId = req.params.id
+        const user = await User.findById(userId)
+        if (!user) {
+            return res.redirect('/')
+        }
+        user.pantry.pull({ _id: itemId })
+        await user.save()
+        res.redirect(`/users/${userId}/foods`)     
+    }catch (error) {
+        console.error(error)
+        res.redirect('/')   
+    }
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
-const app = express();
+
 
 const authController = require('./controllers/auth.js');
 const isSignedIn = require('./middleware/is-signed-in.js');
